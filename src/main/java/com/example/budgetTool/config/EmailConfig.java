9f912de1613c7df58322b972ab250b13ebd2bd8d
KimJaeHyeong <kimjaehyeong92@gmail.com>
2026-04-07 12:33:15 +0900

    logging annotation added

--- src/main/java/com/example/budgetTool/config/EmailConfig.java
+++ src/main/java/com/example/budgetTool/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.example.budgetTool.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ import java.util.Properties;
  * 10/21/25        kimjaehyeong       created
  */
 @Configuration
+@Slf4j
 public class EmailConfig {
     @Value("${spring.mail.host}")
     private String host;
